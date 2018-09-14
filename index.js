e13cda5aa7385f1a5fbdbd28b8715f939e2503f7
vibhu mahendru <vibhumahendru@gmail.com>
2018-09-14 01:14:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,35 +1,40 @@
 var katzDeliLine = [];
-
-function takeANumber (line, name){
-  line.push(name)
-  return `Welcome, ${name}. You are number ${line.length} in line.`;
+var x = 0
+function takeANumber (line){
+  x++
+  line.push(x)
+  return `You are number ${x}.`;
 }
+
+console.log(takeANumber(katzDeliLine))
+console.log(takeANumber(katzDeliLine))
+console.log(takeANumber(katzDeliLine))
+console.log(nowServing(katzDeliLine))
+console.log(nowServing(katzDeliLine))
+console.log(takeANumber(katzDeliLine))
 
 function nowServing(line) {
   if (line.length > 0){
-    
     return `Currently serving ${line.shift()}.`
-    
   }
-  else {return "There is nobody waiting to be served!"
-    
-  }
-  
+  return "There is nobody waiting to be served!"
 }
 
-var linn = []
-function currentLine (line){
- if (line.length>0){
-  for (var i = 0; i<line.length ; i++){
-    linn.push(` ${i+1}. ${line[i]}`)
+
+function currentLine (line) {
+  var line1 = []
+  if (line.length === 0){
+    return "The line is currently empty."
+    
   }
-  return "The line is currently:" + linn
-}
-else{
-  return "The line is currently empty."
-}
+  for (var i = 0;i<line.length;i++){
+    line1.push(` ${i+1}. ${line[i]}`)
+  }
+  return `The line is currently:${line1}`
 }
 
+console.log(currentLine(["Kayla", "Adam", "Jo"]))
+console.log(currentLine(["Kayla", "Adam", "Jo"]))
 
 
 
